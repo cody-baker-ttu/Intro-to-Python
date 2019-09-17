9639845c0373ca601598a50e6ea9f3d68ab6ffdd
cody-baker-ttu <54594606+cody-baker-ttu@users.noreply.github.com>
2019-09-17 10:13:28 -0500

    Create Bats.py

---
+++ Bats.py
@@ -0,0 +1,14 @@
+
+species1 = input("Enter Myotis austroriparius: ")
+string1 = species1[0:3]
+string2 = species1[7:10]
+string3 = string1.upper() + string2.upper()
+print(string3)
+species2 = input("Enter Myotis septentrionalis: ")
+string4 = species2[0:3]
+string5 = species2[7:10]
+string6 = string4.upper() + string5.upper()
+species3 = input("Enter Eptesicus fuscus: ")
+string7 = species3[0:3]
+string8 = species3[10:13]
+string9 = string7.upper() + string8.upper()
