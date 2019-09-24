6cb1372adbcdb0514ad533943b6b09bea6d6ae93
cody-baker-ttu <54594606+cody-baker-ttu@users.noreply.github.com>
2019-09-24 11:22:15 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,15 +2,17 @@ import random
 moves = [ "Rock", "Paper", "Scissors"]
 computer = random.choices( moves, k = 1)
 user = input("Please enter Rock, Paper, or Scissors. ")
-if user=="Rock" and computer=="Scissors":
-        print("You Win!")
-        else:
-        print("You Lose!")
-if user=="Scissors" and computer=="Paper":
-        print("You Win!")
-        else:
-        print("You lose!")
-if user=="Paper"  and computer=="Rock":
+if(user=="Rock" and computer=="Scissors"):
         print("You win!")
-        else:
-        print("You lose!")
+elif(user=="Scissors" and computer=="Paper"):
+        print("You Win!")
+elif(user=="Paper" and computer=="Rock"):
+        print("You win!")
+elif(user=="Paper" and computer=="Scissors")
+        print("You lose.")
+elif(user=="Rock" and computer=="Paper")
+        print("You lose.")
+elif(user=="Scissors" and computer=="Rock")
+        print("You lose.")
+else:
+        print("There is a tie.")
